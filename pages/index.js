50a4df501a9f7cbaf5d7c6d9159ab96b10f7fd9d
Davi Silva <davisds.cv@hotmail.com>
2026-02-18 22:43:33 +0000

    add um emeticon para teste do deploy versel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-     return <h1>Guiga, cheira o meu saco suado, hoje está 36 graus aqui!</h1>
+     return <h1>Guiga, cheira o meu saco suado, hoje está 36 graus aqui!😎</h1>
 }
 
 export default Home;
