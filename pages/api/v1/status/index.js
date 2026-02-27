89f0093538ab477d3c2155bf1514b8b4b369de81
Davi Silva <davisds.cv@hotmail.com>
2026-02-27 19:57:33 +0000

    add o database configuration to .env and update the compose.yaml and database.js to use the environment variables

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
-function status(request, response) {
-  response.status(200).json({
-    chave: `são acima da média, mas não se preocupe, é normal em dias de verão!`,
-  });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "oi papai" });
 }
 export default status;
